cc7c188357e96020db49d48392949e32529dddf7
keeler309 <kylervong455@gmail.com>
2024-07-16 15:42:17 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -11,3 +11,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.marker([35.2176665, -80.831473],{
+  title: "Hello from CPCC!!"
+}).addTo(map);
